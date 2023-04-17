46fab179ba528974214a8049ca5fe120a66e31fa
Abdul-azeez-sk <128384175+Abdul-azeez-sk@users.noreply.github.com>
2023-04-18 03:45:34 +0530

    Add files via upload

---
+++ maingpt.js
@@ -0,0 +1,133 @@
+const frame = document.querySelector('.frame');
+const erase = document.querySelector('.erase');
+const rangeClass = document.querySelector('.range-class');
+const color = document.querySelector('.clr');
+const active = document.querySelector('.active');
+const clear = document.querySelector('.clear');
+const rainbow = document.querySelector('.rainbow');
+let rangeVal =rangeClass.value;
+let clr = color.value;
+let isErasing = false;
+let isRandom=false;
+createDivs(rangeVal);
+color.addEventListener("input",()=>{
+    clr=color.value;
+    isRandom=false;
+})
+rangeClass.addEventListener("input", () => {
+  rangeVal = rangeClass.value;
+  clearScreen();
+  createDivs(rangeVal);
+  
+});
+
+erase.addEventListener("click", () => {
+  isErasing = true;
+});
+
+active.addEventListener("click", () => {
+  isErasing = false;
+  isRandom=false;
+});
+rainbow.addEventListener("click",()=>{
+    isRandom=true;
+    isErasing=false;
+})
+
+
+clear.addEventListener("click", () => {
+  clearScreen();
+  createDivs(rangeVal);
+});
+
+function createDivs(rangeVal) {
+  for (let i = 0; i < rangeVal; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.addEventListener("mouseenter", () => {
+      if (isErasing&&!isRandom||isErasing&&isRandom) {
+        newDiv.style.backgroundColor = "transparent";
+      } else if(!isErasing&&!isRandom){
+        newDiv.style.backgroundColor = clr;
+      }else if(!isErasing&&isRandom){
+        newDiv.style.backgroundColor =generateRandomColor();
+      }
+    });
+    frame.appendChild(newDiv);
+    let rootrange=Math.floor(Math.sqrt(rangeVal));
+    frame.style.gridTemplateColumns = `repeat(${rootrange}, 1fr)`;
+    frame.style.gridTemplateRows = `repeat(${rootrange}, 1fr)`;
+  }
+}
+function clearScreen() {
+  while (frame.firstChild) {
+    frame.removeChild(frame.firstChild);
+  }
+  isRandom=false;
+}
+function generateRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+var colors = new Array(
+    [62,35,255],
+    [60,255,60],
+    [255,35,98],
+    [45,175,230],
+    [255,0,255],
+    [255,128,0]);
+  
+  var step = 0;
+  //color table indices for: 
+  // current color left
+  // next color left
+  // current color right
+  // next color right
+  var colorIndices = [0,1,2,3];
+  
+  //transition speed
+  var gradientSpeed = 0.002;
+  
+  function updateGradient()
+  {
+    
+    if ( $===undefined ) return;
+    
+  var c0_0 = colors[colorIndices[0]];
+  var c0_1 = colors[colorIndices[1]];
+  var c1_0 = colors[colorIndices[2]];
+  var c1_1 = colors[colorIndices[3]];
+  
+  var istep = 1 - step;
+  var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+  var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+  var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+  var color1 = "rgb("+r1+","+g1+","+b1+")";
+  
+  var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+  var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+  var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+  var color2 = "rgb("+r2+","+g2+","+b2+")";
+  
+   $('#gradient').css({
+     background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+      background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+    
+    step += gradientSpeed;
+    if ( step >= 1 )
+    {
+      step %= 1;
+      colorIndices[0] = colorIndices[1];
+      colorIndices[2] = colorIndices[3];
+      
+      //pick two new target color indices
+      //do not pick the same as the current one
+      colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+      colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+      
+    }
+  }
+  
+  setInterval(updateGradient,10); 
+  
